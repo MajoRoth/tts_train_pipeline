34eee9a6c3899716430f11506184e2fe00c94b58
amitroth <amitroth@gmail.com>
2023-03-14 16:55:33 +0200

    changed train pipeline

--- train.py
+++ train.py
@@ -1,65 +1,68 @@
 import os
+import sys
 
-# BaseDatasetConfig: defines name, formatter and path of the dataset.
 from TTS.tts.configs.shared_configs import BaseDatasetConfig
-
-output_path = "/Users/amitroth/PycharmProjects/TTS_playground"
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-
-
-dataset_config = BaseDatasetConfig(
-    formatter="ljspeech", meta_file_train="metadata.csv", path=os.path.join(output_path, "LJSpeech-1.1/")
-)
-
 from TTS.tts.configs.glow_tts_config import GlowTTSConfig
-config = GlowTTSConfig(
-    batch_size=32,
-    eval_batch_size=16,
-    num_loader_workers=4,
-    num_eval_loader_workers=4,
-    run_eval=True,
-    test_delay_epochs=-1,
-    epochs=100,
-    text_cleaner="phoneme_cleaners",
-    use_phonemes=True,
-    phoneme_language="en-us",
-    phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
-    print_step=25,
-    print_eval=False,
-    mixed_precision=True,
-    output_path=output_path,
-    datasets=[dataset_config],
-    save_step=1000,
-)
-
-
 from TTS.utils.audio import AudioProcessor
-ap = AudioProcessor.init_from_config(config)
-# Modify sample rate if for a custom audio dataset:
-# ap.sample_rate = 22050
-
-
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
-tokenizer, config = TTSTokenizer.init_from_config(config)
-
 from TTS.tts.datasets import load_tts_samples
-train_samples, eval_samples = load_tts_samples(
-    dataset_config,
-    eval_split=True,
-    eval_split_max_size=config.eval_split_max_size,
-    eval_split_size=config.eval_split_size,
-)
-
 from TTS.tts.models.glow_tts import GlowTTS
-model = GlowTTS(config, ap, tokenizer, speaker_manager=None)
-
-
 from trainer import Trainer, TrainerArgs
-trainer = Trainer(
-    TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
-)
+
+
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise Exception("make sure you supply output_path and data_path as sys arguments")
+
+    output_path = sys.argv[1]
+    data_path = sys.argv[2]
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    dataset_config = BaseDatasetConfig(
+        formatter="ljspeech", meta_file_train="metadata.csv", path=data_path
+    )
+
+    config = GlowTTSConfig(
+        batch_size=32,
+        eval_batch_size=16,
+        num_loader_workers=4,
+        num_eval_loader_workers=4,
+        run_eval=True,
+        test_delay_epochs=-1,
+        epochs=100,
+        text_cleaner="phoneme_cleaners",
+        use_phonemes=True,
+        phoneme_language="en-us",
+        phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+        print_step=25,
+        print_eval=False,
+        mixed_precision=True,
+        output_path=output_path,
+        datasets=[dataset_config],
+        save_step=1000,
+    )
+
+    ap = AudioProcessor.init_from_config(config)
+    # Modify sample rate if for a custom audio dataset:
+    # ap.sample_rate = 22050
+
+    tokenizer, config = TTSTokenizer.init_from_config(config)
+
+    train_samples, eval_samples = load_tts_samples(
+        dataset_config,
+        eval_split=True,
+        eval_split_max_size=config.eval_split_max_size,
+        eval_split_size=config.eval_split_size,
+    )
+
+    model = GlowTTS(config, ap, tokenizer, speaker_manager=None)
+
+    trainer = Trainer(
+        TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
+    )
+
     trainer.fit()
