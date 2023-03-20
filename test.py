27433a621f6dfa32117cbc9346df978e80f7f08c
amitroth <amitroth@gmail.com>
2023-03-20 16:22:52 +0200

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+import glob, os, sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        raise Exception("make sure you supply output_path, data_path and gpu num as sys arguments")
+
+    output_path = sys.argv[1]
+    data_path = sys.argv[2]
+
+    ckpts = sorted([f for f in glob.glob(output_path + "/*/*.pth")])
+    configs = sorted([f for f in glob.glob(output_path + "/*/*.json")])
+    print(ckpts)
+    print(configs)
+
