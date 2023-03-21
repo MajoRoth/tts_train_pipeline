9b2d9140fb26b605cbf3ad50edf8109a7223e613
amitroth <amitroth@gmail.com>
2023-03-21 10:17:19 +0200

    fastspeech2

--- sbatch.sh
+++ sbatch.sh
@@ -8,4 +8,4 @@
 module load cuda/11.3
 module load cudnn
 source /cs/labs/adiyoss/amitroth/tts_train_pipeline/venv/bin/activate.csh
-python3 train.py /cs/labs/adiyoss/amitroth/tts_train_pipeline /cs/dataset/Download/adiyoss/LJ/preprocessed/LJSpeech-1.1 4
+python3 train_fastspeech2.py
