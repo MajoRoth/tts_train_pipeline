b4de8da1cf8f05c1efe997c61c74ebca6ac6065b
amitroth <amitroth@gmail.com>
2023-04-19 00:40:55 +0300

    cuda vis

--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:8,vmem:15g
-#SBATCH --cpus-per-task=80
-#SBATCH --mem=256g
-#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:1,vmem:15g
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=64g
 module load cuda/11.3
 module load cudnn
 source /cs/labs/adiyoss/amitroth/tts_train_pipeline/venv/bin/activate.csh
